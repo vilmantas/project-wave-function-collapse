b61977ea9352bf0fb96b4af815cd5a83aca99ea1
Vilmantas Petrusevičius <15175886+vilmantas@users.noreply.github.com>
2024-04-12 21:31:12 +0300

    Make limit apply for tiles with rotations enabled.

--- WaveFunctionCollapse/LimitStrategy.cs
+++ WaveFunctionCollapse/LimitStrategy.cs
@@ -6,15 +6,17 @@ public static class LimitStrategy
 {
     public static void Process(Grid grid)
     {
-        foreach (var tile in grid.AvailableTiles.Where(x => x.LimitEnabled))
+        foreach (var grouping in grid.AvailableTiles.GroupBy(x => x.Name).Where(x => x.First().LimitEnabled))
         {
-            var count = grid.CollapsedCells().Count(x => x.Options[0] == tile);
+            var tile = grouping.First();
+
+            var count = grid.CollapsedCells().Count(x => x.Options[0].Name == tile.Name);
 
             if (count < tile.Limit) continue;
 
             foreach (var cell in grid.ProcessableCells())
             {
-                cell.Options.RemoveAll(x => x == tile);
+                cell.Options.RemoveAll(x => x.Name == tile.Name);
             }
         }
     }
